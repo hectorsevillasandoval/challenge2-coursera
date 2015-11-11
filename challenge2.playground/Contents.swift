4f6bece59949b9e07ebf817b9ac399051b3feff4
Hector Sevilla <h2d_07@hotmail.com>
2015-11-10 21:52:21 -0600

    If sentence has been completed

--- challenge2.playground/Contents.swift
+++ challenge2.playground/Contents.swift
@@ -2,4 +2,33 @@
 
 import UIKit
 
-var str = "Hello, playground"
+let numbers = 0...100
+
+for i in numbers{
+    if(i == 0){
+        print("\(i)")
+    }else if(i>0 && i%5==0){
+        
+        if(i % 2 == 0){
+            print("\(i) Bingo y Ademas es Par!!!")
+        }else{
+            print("\(i) Bingo!!!")
+        }
+        
+        
+        
+    }else if(i>0 && i%2==0){
+        
+        print("\(i) Par!!!")
+        
+    }else{
+        
+        print("\(i) Impar!!!")
+        
+    }
+    
+    if(i>=40 && i<=60){
+        print("\(i) Viva Swift!!!")
+    }
+
+}
